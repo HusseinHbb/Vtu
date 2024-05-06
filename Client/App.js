695891ab2bb515fdd27cbbe251ef1ca27b7709b8
Harbestt <habeebhuszein@gmail.com>
2024-05-07 00:52:03 +0100

    tailwind config

---
+++ Client/App.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-xl text-blue-500">App</Text>
+    </View>
+  );
+};
+
+export default App;
